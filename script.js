59e9b6b06c3f3c078c193532d1c261abf44123b9
Iago Duque <83548492+iagoduquedeveloper@users.noreply.github.com>
2025-02-25 23:17:16 -0300

    up
    up

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 }
-
+https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css
 window.onscroll = () => {
     menu.classList.remove('bx-x');
     navbar.classList.remove('active');
