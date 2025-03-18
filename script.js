07bb2c9ad553753f38efd6300249bf14b488b400
iagoduque <iagoduquedeveloper@gmail.com>
2025-03-18 01:22:44 -0300

    desing att

--- script.js
+++ script.js
@@ -10,11 +10,3 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navbar.classList.remove('active');
 }
-
-const typed = new Typed('.multiple-text',  {
-    strings: ['Aptidão física', 'Ganho de peso', 'Treinamento de força', 'Perda de gordura', 'Levantamento de peso', 'Corrida'],
-    typeSpeed: 60,
-    backSpeed: 60,
-    backDelay: 1000,
-    loop: true,
- });
