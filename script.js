549755fd1172da890168f31240ebabc6bb4379a6
iagoduque <iagoduquedeveloper@gmail.com>
2025-04-21 14:39:46 -0300

    slide aplicado

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navbar.classList.remove('active');
 }
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next'
+    }
+  });
